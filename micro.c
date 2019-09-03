9a535a6c5a70132cc34bd1b180663c9c316c7127
PRAMOD P M <53473387+pramodpm2@users.noreply.github.com>
2019-09-03 15:53:29 +0530

    Add files via upload

---
+++ micro.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+void main()
+{
+    int t=1,c=0,d=0;
+    scanf("%d",&t);
+    if(t<=10)
+    {
+        while(c<t)
+        {
+            int k,n,i=0,j,a[1000],small=0;
+            scanf("%d",&n);
+            scanf("%d",&k);
+            for(i=0;i<n;i++)
+            {
+                scanf("%d",&a[i]);
+            }
+            small=a[0];
+            for(i=0;i<n;i++)
+            {
+                if(a[i]<small)
+                {
+                    small=a[i];
+                }
+            }
+            if(small==k)
+            {
+                printf("%d",d);
+            }
+            else
+            {
+                while(small<k)
+                {
+                    small++;
+                    d++;
+                }
+                printf("%d",d);
+                small=0;
+                d=0;
+                c++;
+            }
+        }
+    }
+}
